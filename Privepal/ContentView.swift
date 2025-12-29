c5ee8ede6cac6527fd9507089e78b784bbcaf783
Max Hager <68147541+yachty66@users.noreply.github.com>
2025-12-29 12:55:49 +0100

    Initial Commit

---
+++ Privepal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Privepal
+//
+//  Created by Max Hager on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Privepal/PrivepalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivepalApp.swift
+//  Privepal
+//
+//  Created by Max Hager on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PrivepalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
